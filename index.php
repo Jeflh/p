92fd6dd7056c04c9f945e6796123663b2d2d69a9
Emmanuel Fernández <Jeflh21@gmail.com>
2019-08-17 15:14:17 -0500

    correcciones

---
+++ index.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <!--Estilos-->
+    <link rel="stylesheet" href="css/estilos.css" />
+    <link rel="stylesheet" href="owl/owl.carousel.min.css" />
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css" />
+    <!--Fuente-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, user-scalable=no"> 
+    <title>Emmanuel Fernandez</title>
+</head>
+
+<body>
+
+    <a id="whatsapp" href="https://api.whatsapp.com/send?phone=5213320582950" target=_black ><img src="img/whatsapp.png" alt=""></a>
+    <!--Cabecera-->
+    <header>
+        <div class="menu">
+            <div class="contenedor">
+                <p class="logo">EMMANUEL FERNÁNDEZ</p>
+                <img class="menu-icon" src="img/menu.png" alt="">
+                <nav>
+                    <ul class="lista-menu">
+                        <li><a href="#sobre-mi">Sobre mi</a></li>
+                        <li><a href="#servicios">Servicios</a></li>
+                        <li><a href="#mis-trabajos">Mis trabajos</a></li>
+                        <li><a href="#por-que-trabajar">¿Por qué trabajar conmigo?</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#contacto">Contacto</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div class="contenedor" id="contenedor-titulo-flex">
+            <div class="contenedor-titulo">
+                <h1>Emmanuel Fernández</h1>
+                <h2>Diseñador & Desarrollador Web</h2>
+                <a href="">VER MIS TRABAJOS</a>
+            </div>
+        </div>
+    </header>
+    <!--Cuerpo principal-->
+    <main>
+        <section id="sobre-mi">
+            <div class="contenedor">
+                <h3>Sobre mi</h3>
+                <div class="contenedor-sobremi">
+                    <div class="computadoras">
+                        <img src="img/computadoras.png" alt="Computadora" />
+                    </div>
+                    <div class="texto">
+                        <p>
+                            Mi nombre es Emmanuel Fernández y soy diseñador de páginas web.
+                            Tengo la experiencia en desarrollo de sitios web para pequeñas y
+                            medianas empresas, hasta tiendas en línea con carrito de
+                            compras.
+                        </p>
+                        <p>
+                            Me aseguro de utilizar siempre las últimas tecnologías de
+                            desarrollo web: HTML5, CSS3, jQuery, Wordpress. Si necesitás una
+                            página web moderna, funcional, que se adapte a todos los
+                            diferentes tipos de pantalla, te aseguro que llegaste al lugar
+                            correcto.
+                        </p>
+                        <p>
+                            Podes ponerte en contacto conmigo a través de Facebook, Linkedin
+                            o completando el formulario de contacto que se encuentra al
+                            final de mi sitio web.
+                        </p>
+                        <a href="">VER MIS TRABAJOS</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--Servicios-->
+        <section id="servicios">
+            <div class="contenedor">
+                <h3>Servicios</h3>
+                <div class="contenedor-servicios">
+                    <div class="servicio violeta">
+                        <h4>Diseño Web</h4>
+                        <p>
+                            Páginas web atractivas y funcionales con el objetivo de
+                            satisfacer todas tus expectativas. La importancia del diseño es
+                            fundamental para tener éxito y atraer a tus visitantes. También
+                            lo es la velocidad de carga del sitio web, deben ser rápidos
+                            para que el visitante tenga la mejor experiencia.
+                        </p>
+                        <img class="icono" src="img/monitor.png" />
+                        <img class="ondas" src="img/waves.png" />
+                    </div>
+                    <div class="servicio celeste">
+                        <h4>Responsive Desing</h4>
+                        <p>
+                            Hoy en día las páginas web se ven en multitud de dispositivos
+                            como tablets, celulares, libros electrónicos, notebooks, PCs,
+                            etc. La tecnología móvil es el presente y el futuro. Para eso es
+                            importante y me aseguro de que tu sitio web sea compatible con
+                            todos los tamaños de pantalla de esta época moderna.
+                        </p>
+                        <img class="icono" src="img/mobile.png" />
+                        <img class="ondas" src="img/waves.png" />
+                    </div>
+                    <div class="servicio violeta">
+                        <h4>SEO</h4>
+                        <p>
+                            Al crear sitios web, tengo muy en cuenta la optimización de
+                            motores de búsqueda (SEO). Llevo a cabo prácticas de SEO
+                            estándar en cada página del sitio web, lo que permite que tu
+                            sitio tenga una clasificación más alta en los resultados de
+                            Google y asi generar mas tráfico a la web.
+                        </p>
+                        <img class="icono" src="img/seo.png" />
+                        <img class="ondas" src="img/waves.png" />
+                    </div>
+                    <div class="servicio celeste">
+                        <h4>Autoadministrable</h4>
+                        <p>
+                            Es importante que puedas administrar tu página web vos mismo,
+                            desde cualquier lugar y en cualquier momento. Desarrollo
+                            sistemas de gestión de contenido que te permiten ver los datos
+                            de tu página web y editar el contenido que quieras, lo que te
+                            permite mantenerte actualizado.
+                        </p>
+                        <img class="icono" src="img/wordpress.png" />
+                        <img class="ondas" src="img/waves.png" />
+                    </div>
+                    <div class="servicio violeta">
+                        <h4>Mantenimiento</h4>
+                        <p>
+                            Podés contactarme si necesitas actualizar tu página web, agregar
+                            nuevas secciones o cualquier cambio que necesites. Esto es
+                            perfecto para los sitios web de pequeñas empresas, donde no se
+                            necesita un diseñador de páginas web a tiempo completo, sino
+                            solo en distintas ocasiones.
+                        </p>
+                        <img class="icono" src="img/mante.png" />
+                        <img class="ondas" src="img/waves.png" />
+                    </div>
+                    <div class="servicio celeste">
+                        <h4>Marketing Digital</h4>
+                        <p>
+                            Utilizo las herramientas mas importantes como Google Adwords y
+                            Google Analytics, para que tu negocio aparezca en los resultados
+                            de búsqueda de Google justo cuando alguien te necesita y
+                            analizar como los usuarios interactúan con tu página web.
+                        </p>
+                        <img class="icono" src="img/marketing.png" />
+                        <img class="ondas" src="img/waves.png" />
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--Mis trabajos-->
+        <section id="mis-trabajos">
+            <div class="contenedor">
+                <h3>Mis Trabajos</h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="item"><img src="img/sassy.jpg"></div>
+                    <div class="item"><img src="img/chaia.jpg"></div>
+                    <div class="item"><img src="img/alimentar.jpg"></div>
+                    <div class="item"><img src="img/isip.jpg"></div>
+                    <div class="item"><img src="img/chaia.jpg"></div>
+                </div>
+        </section>
+
+        <!--Por que trabajar conmigo-->
+        <section id="por-que-trabajar">
+            <div class="contenedor">
+                <h3>¿Por que trabajar conmigo?</h3>
+                <div class="contenedor-trabajar">
+                    <div class="item">
+                        <ul>
+                            <li><img src="img/velocidad.png" alt=""></li>
+                            <li>Velocidad</li>
+                            <li>Me aseguro de que tu web cargue entre 2 y 4 segundos (depende de la cantidad de
+                                contenido del sitio).
+                                Si una web tarda mas de 5 segundos en cargar, el 70% de los usuarios lo cierra y no
+                                vuelve a entrar más.
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="item">
+                        <ul>
+                            <li><img src="img/mantenimiento.png" alt=""></li>
+                            <li>Mantenimiento</li>
+                            <li>Mantengo una comunicación constante con mi cliente, para enseñarle a modificar contenido
+                                mediante el
+                                panel de Wordpress o para cualquier duda/cambio que quiera hacer a su sitio web.
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="item">
+                        <ul>
+                            <li><img src="img/mobile2.png" alt=""></li>
+                            <li>Mobile Priority</li>
+                            <li>Cada vez más usuarios navegan desde sus celulares en cualquier parte del mundo. Me
+                                aseguro de que tu
+                                web esté optimizada para cualquier tipo de dispositivo móvil.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--Blog-->
+        <section id="blog">
+            <div class="contenedor">
+                <h3>Blog</h3>
+                <div class="contenedor-publicaciones">
+                    <div class="publicacion">
+                        <img src="img/blog1.jpg" alt="">
+                        <div class="contenido">
+                            <h4>No alcanza con tener solamente una página web.</h4>
+                            <p>¿Quieres aprender cuales son las herramientas que usan las mejores marcas para generar
+                                tráfico?</p>
+                            <a href="">Leer más →</a>
+                        </div>
+                    </div>
+                    <div class="publicacion">
+                        <img src="img/blog2.jpg" alt="">
+                        <div class="contenido">
+                            <h4>¿Qué es el remarketing y como utilizarlo?</h4>
+                            <p>El remarketing es una de las técnicas mas utilizadas por las mejores
+                                companías!<br>¿Quieres saber de
+                                que se trata?</p>
+                            <a href="">Leer más →</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="que-esperas">
+            <div class="contenedor">
+                <h3>¿Qué esperas para tener tu página web?</h3>
+                <a href="">HABLEMOS</a>
+            </div>
+        </section>
+
+        <!--Contacto-->
+        <section id="contacto">
+            <?php
+      if(@$_GET['i']=='ok') { // QUIERE DECIR QUE EL FORMULARIO SE ENVIO CORRECTAMENTE ?>
+
+            <h3>La consulta se envio correctamente. Nos contactaremos a la brevedad.</h3>
+
+            <?php
+      } else{
+      ?>
+            <div class="contenedor">
+                <h3>Contacto</h3>
+                <form action="envio-formulario.php" method="post" enctype="multipart/form-data">
+                    <input type="text" placeholder="Nombre" name="nombre" required>
+                    <input type="email" placeholder="Email" name="email" required>
+                    <textarea placeholder="Mensaje" name="mensaje" required></textarea>
+                    <input type="submit" value="ENVIAR MENSAJE">
+                </form>
+                <?php } ?>
+
+                <div class="contacto-info">
+                    <div class="mail"><img src="img/mail-icon.png" alt="">Jeflh21@gmail.com</div>
+                    <div class="whatsapp"><img src="img/whatsapp-icon.png" alt="">3320582950</div>
+                </div>
+        </section>
+    </main>
+    <!--Pie de página-->
+    <footer>
+        <div class="contenedor">
+            <div class="redes">
+                <a href="https://www.facebook.com/emmanuel.fernandez.587606"><img src="img/facebook.png" alt="imagen de facebook"></a>
+                <a href="https://github.com/Jeflh"><img src="img/github.png" alt="imagen de github"></a>
+                <a href="https://twitter.com/Emmanuell_21"><img src="img/twitter.png" alt="imagen de twitter"></a>
+            </div>
+            <div class="parrafo">
+                <p>&copy;2019 Emmanuel Fernández | Diseñador Web México. Todos los derechos reservados.</p>
+            </div>
+        </div>
+    </footer>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="jquery.min.js"></script>
+<script src="owl/owl.carousel.min.js"></script>
+<script>
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+</script>
+
+<script>
+
+$(document).ready(function(){
+
+$(window).scroll(function(){
+  scroll = $(window).scrollTop();
+  if (scroll > 100) {
+    $('.menu').css({"position":"fixed"});
+    $('.menu').css({"width":"100%"});
+    $('.menu').css({"top":"0"});
+    $('.menu').css({"background":"#fff"});
+    $('.menu a').css({"color":"#000"});
+    $('.logo').css({"color":"#000"});
+    $('.menu').css({"box-shadow":"rgba(0, 0, 0, 0.22) 6px 1px 1px"});
+    $('.menu').css({"z-index":"100"});
+  } else {
+    $('.menu').css({"position":"relative"});
+    $('.menu').css({"background":"transparent"});
+    $('.menu').css({"box-shadow":"0 0 0"});
+    $('.menu a').css({"color":"#fff"});
+    $('.logo').css({"color":"#fff"});
+  }
+})
+
+$('.menu-icon').click(function(){
+  $('header nav').slideToggle();
+})
+
+})
+
+</script>  
+
+</body>
+
+</html>
